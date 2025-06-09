721e2578382f5b3ee4dde5f5f70b28404c82e583
Elisson <elisvec8@gmail.com>
2025-06-09 09:43:48 -0300

    email enviando, mas falta alterar codigo

--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,28 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3001; // diferente da porta do frontend
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/send-reset-code', async (req, res) => {
+app.post('/api/send-reset-code', async (req: Request, res: Response): Promise<void> => {
   const { email } = req.body;
 
   if (!email) {
-    return res.status(400).json({ message: "E-mail é obrigatório" });
+    res.status(400).json({ message: "E-mail é obrigatório" });
+    return;
   }
 
-  const resetCode = Math.floor(100000 + Math.random() * 900000); // Código de 6 dígitos
+  const resetCode = Math.floor(100000 + Math.random() * 900000);
 
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: 'elissonvictorc@gmail.com',
-      pass: 'rntb guue dzls uokq', // precisa ser uma senha de aplicativo
+      pass: 'ursr vjns whqn itnc', // senha de app
     },
   });
 
